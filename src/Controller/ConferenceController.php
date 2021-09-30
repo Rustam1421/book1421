b1c36686cd49f404bd56fddebd30268c789654b1
rustam1421 <r.r51114@gmail.com>
2021-09-30 23:12:06 +0300

    changes

---
+++ src/Controller/ConferenceController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Conference;
+use App\Repository\CommentRepository;
+use App\Repository\ConferenceRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ConferenceController extends AbstractController
+{
+
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function index(ConferenceRepository $conferenceRepository): Response
+    {
+        return $this->render('conference/index.html.twig', [
+            'conferences' => $conferenceRepository->findAll()
+        ]);
+    }
+
+    /**
+     * @Route("/conference/{id}", name="conference")
+     */
+    public function show(Conference $conference,  CommentRepository $commentRepository, Request $request): Response
+    {
+        $offset = max(0, $request->query->getInt('page', 0));
+        $paginator = $commentRepository->getCommentPaginator($conference, $offset);
+
+        return $this->render('conference/show.html.twig', [
+            'conference' => $conference,
+            'comments' => $paginator,
+            'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
+            'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
+        ]);
+    }
+}
